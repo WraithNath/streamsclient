4eaa23a29583c63acfdb71b6231ab5f3345072d2
Nathan Freeman-Smith <wraithnath@gmail.com>
2020-01-13 17:01:47 +0000

    Added stream components and router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-class App extends React.Component{
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+
+class App extends React.Component {
     render = () => {
-        return <div>App</div>;
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/create" exact component={StreamCreate} />
+                        <Route path="/streams/delete" exact component={StreamDelete} />
+                        <Route path="/streams/edit" exact component={StreamEdit} />
+                        <Route path="/streams/show" exact component={StreamShow} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
     }
 }
 
